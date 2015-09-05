0214fa23d1f41bcc916706ef55f77135a6f39767
everdimension <everdimension@gmail.com>
2015-09-06 02:37:55 +0300

    drop jsx-loader in favor of babel-loader (in accordance with README.md and pull request #31)

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'jsx-loader?harmony' },
+      { test: /\.js$/, loader: 'babel-loader' },
       { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
